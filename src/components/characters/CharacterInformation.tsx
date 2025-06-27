65be0eba2c85220410e0efc1c343d40701b747c0
Yevhenii Kachan <djianosis@gmail.com>
2025-06-27 12:09:28 +0300

    Upadte style for Home page

--- src/components/characters/CharacterInformation.tsx
+++ src/components/characters/CharacterInformation.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router";
 import classes from "./CharacterInformation.module.scss";
 import type { Character } from "./types";
 import Card from "../shared/Card/Card";
+import Button from "../shared/Button/Button";
 
 type CharacterInformationProps = {
   character: Character;
@@ -11,39 +12,41 @@ export default function CharacterInformation({
   character,
 }: CharacterInformationProps) {
   return (
-    <Card>
-      <div className={classes.information_card}>
-        <div className={classes.framed_image}>
-          <img src={character.image} alt={character.name} />
-        </div>
-        <ul>
-          <li>Name: {character.name}</li>
-          <li>Role: {character.role}</li>
-          <li>Description: {character.description}</li>
-        </ul>
-      </div>
-
-      {character.familiar && (
+    <Card className={classes.character_information}>
+      <div className={classes.information_card_wrapper}>
         <div className={classes.information_card}>
           <div className={classes.framed_image}>
-            <img
-              src={
-                character.familiar.image
-                  ? character.familiar.image
-                  : "/src/assets/images/Jack.jpg"
-              }
-              alt={character.familiar.name}
-            />
+            <img src={character.image} alt={character.name} />
           </div>
           <ul>
-            <li>Familiar: {character.familiar.name}</li>
-            <li>Species: {character.familiar.species}</li>
-            <li>Description: {character.familiar.description}</li>
+            <li>Name: {character.name}</li>
+            <li>Role: {character.role}</li>
+            <li>Description: {character.description}</li>
           </ul>
         </div>
-      )}
+
+        {character.familiar && (
+          <div className={classes.information_card}>
+            <div className={classes.framed_image}>
+              <img
+                src={
+                  character.familiar.image
+                    ? character.familiar.image
+                    : "/src/assets/images/Jack.jpg"
+                }
+                alt={character.familiar.name}
+              />
+            </div>
+            <ul>
+              <li>Familiar: {character.familiar.name}</li>
+              <li>Species: {character.familiar.species}</li>
+              <li>Description: {character.familiar.description}</li>
+            </ul>
+          </div>
+        )}
+      </div>
       <Link to={`/character/${character.id}`}>
-        <button>Подробнее о {character.name}</button>
+        <Button type="button">Details about {character.name}</Button>
       </Link>
     </Card>
   );
